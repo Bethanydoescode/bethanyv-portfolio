2d9a364118e4deb58b016718b7a65e73f906eb86
Bethanydoescode <bethany.mills33@gmail.com>
2025-12-07 22:38:04 -0800

    added see more button

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -8,9 +8,29 @@ export const Projects = () => {
     >
       <RevealOnScroll>
         <div className="max-w-5xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-linear-to-r from-pink-200 to-pink-500 bg-clip-text text-transparent">
-            Featured Projects
-          </h2>
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-bold mb-8 bg-linear-to-r from-pink-200 to-pink-500 bg-clip-text text-transparent">
+              Featured Projects
+            </h2>
+
+            <a
+              href="https://github.com/Bethanydoescode"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-pink-300 hover:text-pink-100 transition flex items-center gap-2 text-sm"
+            >
+              View More
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303..."></path>
+              </svg>
+            </a>
+          </div>
+
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-pink-900/30 hover:shadow-[0_2px_8px_rgba(255, 182, 193, 0.4)] transition">
               <h3 className="text-xl font-bold mb-4">
