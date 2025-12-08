8553f527a6c5ea7e73cdfa5315422593aaeb9b1d
Bethanydoescode <bethany.mills33@gmail.com>
2025-12-07 21:15:16 -0800

    @

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import emailjs from "emailjs/browser";
+import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
   const [formData, setFormData] = useState({
