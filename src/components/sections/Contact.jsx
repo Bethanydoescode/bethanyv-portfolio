d6305d951a3668a29ba8b1034c806e15e7a7041b
Bethanydoescode <bethany.mills33@gmail.com>
2025-12-07 21:30:01 -0800

    Trigger GitHub Actions deploy

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
     email: "",
     message: "",
   });
-  //
+
   //   const serviceId = import.meta.env.VITE_SERVICE_ID;
   //   const templateId = import.meta.env.VITE_TEMPLATE_ID;
   //   const publicKey = import.meta.env.VITE_PUBLIC_KEY;
